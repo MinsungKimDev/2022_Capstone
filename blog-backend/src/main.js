9b4c2c3201017eca89852fc02ec119e0c5555f1a
MinsungKimDev <minsungkim.dev@gmail.com>
2022-06-03 03:08:31 +0900

    Add README and Release Scripts

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const static = require('koa-static');
+const send = require('koa-send');
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
@@ -12,8 +14,13 @@ const port = process.env.SERVER_PORT || 4000;
 
 router.use('/api', api.routes());
 
+app.use(static(__dirname + '/../build'));
 app.use(bodyParser());
 app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
+
+app.use(async (ctx) => {
+    await send(ctx, ctx.path, { root: __dirname + '/../build/index.html' });
+});
 
 app.listen(4000, () => { console.log(`Koa server is listening to port ${port}`); });
